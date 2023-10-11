e81d385982b6d957fee43435973bd4bcf98edc30
lesageethan <lesageethan@gmail.com>
2023-10-11 20:41:25 +0100

    file reader class

---
+++ fileReader.py
@@ -0,0 +1,21 @@
+import PyPDF2
+
+class FileReader():
+    def __init__(self, filename):
+        self.filename = filename
+        self.pdf_contents = self.read_pdf(filename)
+    
+    @staticmethod
+    def read_pdf(filename):
+        pdf_file_object = open(filename, 'rb')
+        pdf_reader = PyPDF2.PdfReader(pdf_file_object)
+        output = ""
+        for i in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[i]
+            output += page.extract_text()
+        return output
+
+f = FileReader("Cambridge IGCSE Textbook.pdf")
+print(f.filename)
+print(f.pdf_contents)
+print(len(f.pdf_contents))
